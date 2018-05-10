06f262ccfa906f3ad185ca9e7a343466dc42f563
Bartosz Drozd <bdrozd@bdrozd-o7010.t1.gda.wp-sa.pl>
2018-05-10 13:53:05 +0200

    Implementacja 6 wymagan

--- src/GreetingBot.php
+++ src/GreetingBot.php
@@ -5,21 +5,104 @@ namespace Kata;
 class GreetingBot
 {
     /**
-     * @var ?string
+     * @var string|array|null
      */
     private $name;
 
-    public function greet(?string $name) : string
+    /**
+     * @param string|array|null $name
+     * @return string
+     */
+    public function greet($name) : string
     {
         $this->name = $name;
 
-        $response = $this->getResponseCall();
-
-        return "Hello, {$response}.";
+        return $this->getResponseCall();
     }
 
     private function getResponseCall() : string
     {
-        return $this->name ?? "my friend";
+        if (is_array($this->name)) {
+            return $this->getResponseForMultipleNames();
+        }
+        return $this->getResponseForSingleName();
+    }
+
+    private function getResponseForMultipleNames() : string
+    {
+        $shoutedName = $this->getShoutedNameIfGiven();
+
+        if ($shoutedName) {
+            $this->removeShoutedNameFromAllNames($shoutedName);
+        }
+
+        if (count($this->name) > 2) {
+            $response = $this->getResponseForMoreThanTwoNames();
+        } else {
+            $response = $this->getResponseForExactlyTwoNames();
+        }
+
+        return $shoutedName ? ($response . " AND {$this->getResponseForShoutedName($shoutedName)}") : $response;
+    }
+
+    private function getShoutedNameIfGiven() : ?string
+    {
+        foreach ($this->name as $name) {
+            if (ctype_upper($name)) {
+                return $name;
+            }
+        }
+        return null;
+    }
+
+    private function removeShoutedNameFromAllNames(string $shoutedName) : void
+    {
+        $shoutedNameIndex = array_search($shoutedName, $this->name);
+
+        unset($this->name[$shoutedNameIndex]);
+
+        $this->name = array_values($this->name);
+    }
+
+    private function getResponseForMoreThanTwoNames() : string
+    {
+        $firstNames = array_slice($this->name, 0, count($this->name) - 1);
+        $lastName = end($this->name);
+        $names = implode(", ", $firstNames) . ", and {$lastName}";
+
+        return "Hello, {$names}.";
+    }
+
+    private function getResponseForExactlyTwoNames() : string
+    {
+        $names = implode(" and ", $this->name);
+
+        return "Hello, {$names}.";
+    }
+
+    private function getResponseForSingleName() : string
+    {
+        if ($this->name) {
+            return $this->getResponseForGivenName();
+        }
+        return $this->getResponseForNullName();
+    }
+
+    private function getResponseForGivenName() : string
+    {
+        if (ctype_upper($this->name)) {
+            return $this->getResponseForShoutedName($this->name);
+        }
+        return "Hello, {$this->name}.";
+    }
+
+    private function getResponseForShoutedName(string $name) : string
+    {
+        return "HELLO {$name}!";
+    }
+
+    private function getResponseForNullName() : string
+    {
+        return "Hello, my friend.";
     }
 }
--- tests/GreetingBotTest.php
+++ tests/GreetingBotTest.php
@@ -20,11 +20,51 @@ class GreetingBotTest extends TestCase
     /**
      * @test
      */
-    public function itAllowsNullNamesByIntroducingStandIn()
+    public function itHandlesNullName()
     {
         $answer = $this->getAnswerForName(null);
 
         $this->assertContains("my friend", $answer);
+    }
+
+    /**
+     * @test
+     */
+    public function itHandlesShoutingWhenGivenNameIsAllUppercase()
+    {
+        $answer = $this->getAnswerForName("BART");
+
+        $this->assertContains("HELLO BART!", $answer);
+    }
+
+    /**
+     * @test
+     */
+    public function itHandlesExactlyTwoNames()
+    {
+        $answer = $this->getAnswerForMultipleNames(["Bart", "Amy"]);
+
+        $this->assertContains("Bart and Amy", $answer);
+    }
+
+    /**
+     * @test
+     */
+    public function itHandlesMoreThanTwoNames()
+    {
+        $answer = $this->getAnswerForMultipleNames(["Bart", "Amy", "Nicole"]);
+
+        $this->assertContains("Bart, Amy, and Nicole", $answer);
+    }
+
+    /**
+     * @test
+     */
+    public function itHandlesMultipleNamesBothNormalAndShouted()
+    {
+        $answer = $this->getAnswerForMultipleNames(["Bart", "AMY", "Nicole"]);
+
+        $this->assertContains("Bart and Nicole. AND HELLO AMY!", $answer);
     }
 
     private function getAnswerForName(?string $name) : string
@@ -32,5 +72,12 @@ class GreetingBotTest extends TestCase
         $bot = new GreetingBot();
 
         return $bot->greet($name);
+    }
+
+    private function getAnswerForMultipleNames(array $names) : string
+    {
+        $bot = new GreetingBot();
+
+        return $bot->greet($names);
     }
 }
